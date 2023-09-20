1a98f91c22af1eda9c6b5d3dead7e454f33533b4
SeongkookKIM <ksk8646@naver.com>
2023-09-20 17:54:49 +0900

    체크박스 한 개만 선택가능하게 기능 구현

--- lonely/src/components/Form.tsx
+++ lonely/src/components/Form.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiX } from "react-icons/fi";
 
 function Form() {
+  const [selectedAnswer, setSelectedAnswer] = useState<string>("");
+
+  const handleAnswerChange = (e: any) => {
+    const { name, value, checked } = e.target;
+
+    if (checked) {
+      setSelectedAnswer(value);
+    } else if (selectedAnswer === value) {
+      setSelectedAnswer("");
+    }
+  };
+
   return (
     <div className="form">
       <form>
@@ -32,23 +44,63 @@ function Form() {
           <label>ANSWER</label>
           <div className="answer-box">
             <div>
-              <input type="checkbox" name="answer" value="Bitcoin" />
+              <input
+                type="checkbox"
+                name="answer"
+                value="Bitcoin"
+                checked={selectedAnswer === "Bitcoin"}
+                onChange={(e) => {
+                  handleAnswerChange(e);
+                }}
+              />
               <span>Bitcoin</span>
             </div>
             <div>
-              <input type="checkbox" name="answer" value="Bitcoin Classic" />
+              <input
+                type="checkbox"
+                name="answer"
+                value="Bitcoin Classic"
+                checked={selectedAnswer === "Bitcoin Classic"}
+                onChange={(e) => {
+                  handleAnswerChange(e);
+                }}
+              />
               <span>Bitcoin Classic</span>
             </div>
             <div>
-              <input type="checkbox" name="answer" value="Ethereum" />
+              <input
+                type="checkbox"
+                name="answer"
+                value="Ethereum"
+                checked={selectedAnswer === "Ethereum"}
+                onChange={(e) => {
+                  handleAnswerChange(e);
+                }}
+              />
               <span>Ethereum</span>
             </div>
             <div>
-              <input type="checkbox" name="answer" value="Tether" />
+              <input
+                type="checkbox"
+                name="answer"
+                value="Tether"
+                checked={selectedAnswer === "Tether"}
+                onChange={(e) => {
+                  handleAnswerChange(e);
+                }}
+              />
               <span>Tether</span>
             </div>
             <div>
-              <input type="checkbox" name="answer" value="Cardano" />
+              <input
+                type="checkbox"
+                name="answer"
+                value="Cardano"
+                checked={selectedAnswer === "Cardano"}
+                onChange={(e) => {
+                  handleAnswerChange(e);
+                }}
+              />
               <span>Cardano</span>
             </div>
           </div>
